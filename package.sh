573598f5b7aec254b39bea7190dc0d56a1a4b3bc
Thomas Smallwood <thomasinio@hotmail.com>
2022-10-08 13:25:45 +0100

    check ecr login before build

--- package.sh
+++ package.sh
@@ -1,9 +1,11 @@
 # check CLIs
 aws --version && docker --version || exit 1
 
+# docker login
+aws ecr get-login-password --region eu-west-2 --profile 099867171230_TerrariaAdmin | docker login -u AWS --password-stdin 099867171230.dkr.ecr.eu-west-2.amazonaws.com || exit 1
+
 # build
 docker build ./ --file Dockerfile -t terraria-server:latest --tag "terraria-server" || exit 1
 
 # push
-aws ecr get-login-password --region eu-west-2 --profile 099867171230_TerrariaAdmin | docker login -u AWS --password-stdin 099867171230.dkr.ecr.eu-west-2.amazonaws.com
 docker push 099867171230.dkr.ecr.eu-west-2.amazonaws.com/terraria-server:latest
